e32d5f335c4ed372d3d0b8695e647ca5947d333c
buddhaman <tim.trussner@gmail.com>
2023-08-16 13:54:04 +0200

    Wrote simple lexer for single character symbols and tested them

--- cmd/lexer.go
+++ cmd/lexer.go
@@ -7,11 +7,51 @@ type Lexer struct {
 	ch           byte
 }
 
-func New(input string) *Lexer {
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func NewLexer(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
-func NewThing(fuckyou string) string {
-	return fuckyou + "hi"
+func (l *Lexer) NextToken() Token {
+	var tok Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(ASSIGN, l.ch)
+	case ';':
+		tok = newToken(SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(LPAREN, l.ch)
+	case ')':
+		tok = newToken(RPAREN, l.ch)
+	case ',':
+		tok = newToken(COMMA, l.ch)
+	case '+':
+		tok = newToken(PLUS, l.ch)
+	case '{':
+		tok = newToken(LBRACE, l.ch)
+	case '}':
+		tok = newToken(RBRACE, l.ch)
+	case 0:
+		tok.Type = EOF
+		tok.Literal = ""
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType TokenType, ch byte) Token {
+	return Token{Type: tokenType, Literal: string(ch)}
 }
--- cmd/lexer_test.go
+++ cmd/lexer_test.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLexer(t *testing.T) {
+	path := "tok_test.butter"
+	fmt.Println("Start reading file", path)
+
+	/*
+		fileBytes, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Print("Could not read file.")
+		}
+	*/
+
+	file := "((()))+++"
+
+	l := NewLexer(file)
+
+	var tok Token
+
+	for {
+		tok = l.NextToken()
+		name := TokStr(tok.Type)
+		fmt.Println("Just read", name, tok.Literal)
+		if tok.Type == EOF {
+			break
+		}
+	}
+}
